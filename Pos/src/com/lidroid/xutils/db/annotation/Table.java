b92381d778682b7ce5decb5118c17a75c20eeed6
zhizun <jianyezhizun@sina.com>
2015-09-17 19:53:25 +0800

    第三次更新

---
+++ Pos/src/com/lidroid/xutils/db/annotation/Table.java
@@ -0,0 +1,15 @@
+package com.lidroid.xutils.db.annotation;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({java.lang.annotation.ElementType.TYPE})
+public @interface Table
+{
+  String execAfterTableCreated() default "";
+  
+  String name() default "";
+}
